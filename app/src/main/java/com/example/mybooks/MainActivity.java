5c07aa9c15103b97cf9a16f8257115a8d06e499c
KeilaSuellen <keilasuellen02@gmail.com>
2022-06-15 20:22:57 -0300

    criando os construtores.

--- app/src/main/java/com/example/mybooks/MainActivity.java
+++ app/src/main/java/com/example/mybooks/MainActivity.java
@@ -6,13 +6,24 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
    RecyclerView ListaLivros;
+
+   List<Books> books;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ListaLivros=findViewById(R.id.idRecyvlearwiewLivros);
+
+        //criando lista
+        books = new ArrayList<>();
+
+
 
         //implementando o Layout que será utilizado
         ListaLivros.setLayoutManager(new GridLayoutManager(getApplicationContext(),3));
